2ffb3eb475c3c80adc26834a8601a74122a31225
AngelinaMarkova99 <31545084+AngelinaMarkova99@users.noreply.github.com>
2019-07-06 11:18:31 +0300

    Update Edit_window.java

--- AlgorithmBoruvki/src/Edit_window.java
+++ AlgorithmBoruvki/src/Edit_window.java
@@ -10,7 +10,7 @@ public class Edit_window extends JFrame{
     private JButton deleteGraph = new JButton("Удалить граф");
     private JButton saveGraph = new JButton("Сохранить граф");
     private JButton addData = new JButton("Добавить");
-    private JLabel label = new JLabel("Откуда:        Куда:        Вес ребра:");
+    private JLabel label = new JLabel("Откуда:        Куда:       Вес ребра:");
     private JTextArea listData = new JTextArea(); //список добавленных вершин и рёбер
     private JTextField inputLine = new JTextField("",3);
     private JPanel editPanel = new JPanel(); //окно редактирования
